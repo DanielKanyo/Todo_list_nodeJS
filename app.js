5093fdc17a0465b5eb5d8226271e275b3ea7f8dd
DanielKanyo <danielkanyo992@gmail.com>
2017-08-01 20:30:38 +0200

    The main files

---
+++ app.js
@@ -0,0 +1,21 @@
+//app.js is the launcher engine
+
+var express = require('express');
+var todoController = require('./controllers/todoController');
+
+var app = express();
+
+//set up template engine
+//the project knows that we gonna use ejs for our templates
+app.set('view egine', 'ejs'); 
+
+//static files
+//middleware
+app.use(express.static('./public'));
+
+//fire controllers
+todoController(app);
+
+//listen to port and log
+app.listen(3000);
+console.log('You are listening to port 3000!');
